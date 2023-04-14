0dee9f76adb97717fa2cf611acd820ef227c0512
Nikoloz Shervashidze <51024662+nsher888@users.noreply.github.com>
2023-04-14 20:25:56 +0400

    style: add responsive styles

--- resources/views/components/form-header.blade.php
+++ resources/views/components/form-header.blade.php
@@ -1,4 +1,4 @@
 @props(['title', 'subtitle'])
 
-<h1 class="mb-4 text-2xl font-extrabold">{{ $title }}</h1>
-<p class="mb-6 text-lg text-zinc-500">{{ $subtitle }}</p>
+<h1 class="mb-4 text-xl font-extrabold md:text-2xl">{{ $title }}</h1>
+<p class="mb-6 md:text-lg text-zinc-500">{{ $subtitle }}</p>
--- resources/views/components/split-layout.blade.php
+++ resources/views/components/split-layout.blade.php
@@ -1,13 +1,12 @@
 <div class="grid min-h-screen grid-cols-5">
-    <div class="col-span-3 py-10 px-28">
+    <div class="col-span-5 px-4 py-10 md:px-28 md:col-span-3">
         <a href="">
-            <img src="{{ asset('images/coronatime-logo.png') }}" alt="Page logo" class="mb-14">
-
+            <img src="{{ asset('images/coronatime-logo.png') }}" alt="Page logo" class="inline-block mb-10 md:mb-14">
         </a>
         {{ $slot }}
     </div>
 
-    <div class="col-span-2">
+    <div class="hidden col-span-2 md:block">
         <img class="w-full h-full max-h-screen" src="{{ asset('images/bg.png') }}" alt="Background">
     </div>
 </div>
