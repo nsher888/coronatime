f9fda22662609affbec0bf6fb061efc14589a8a9
Nikoloz Shervashidze <51024662+nsher888@users.noreply.github.com>
2023-04-14 23:48:38 +0400

    feat: add logout functionality

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,16 @@
+<div class="border-b-2">
+    <div class="flex items-center justify-between py-5 mx-auto max-w-7xl">
+        <div>
+            <img src="{{ asset('images/logo-2.png') }}" alt="Logo">
+        </div>
+
+        <div class="flex gap-5">
+            <p>{{ auth()->user()->username }}</p>
+
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                <button type="submit">Log out</button>
+            </form>
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,8 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/register', [RegisterController::class, 'create'])->name('register.create');
-Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+Route::get('/register', [RegisterController::class, 'create'])->name('register.create')->middleware('guest');
+Route::post('/register', [RegisterController::class, 'store'])->name('register.store')->middleware('guest');
+
+Route::post('/logout', [SessionController::class, 'destroy'])->name('logout')->middleware('auth');
 
 Route::get('/home', function () {
     return view('home');
