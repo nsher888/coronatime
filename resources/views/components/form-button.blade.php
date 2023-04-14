f7621a5db4cf98605a12fbaeb3f473bf6ba253ab
Nikoloz Shervashidze <51024662+nsher888@users.noreply.github.com>
2023-04-14 19:37:42 +0400

    style: add registration page markup

--- resources/views/components/form-button.blade.php
+++ resources/views/components/form-button.blade.php
@@ -2,4 +2,4 @@
 
 <button
     class="w-full py-4 mb-6 font-extrabold text-white transition-all duration-100 bg-green-500 rounded-lg hover:bg-green-600">{{
-    $label }}</button>
+    strtoupper($label) }}</button>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,7 +9,7 @@
                 <a href="" class="text-sm font-semibold text-blue-700">Forgot password?</a>
             </div>
 
-            <x-form-button label="LOG IN" />
+            <x-form-button label="log in" />
             <div class="flex self-center justify-center gap-2">
                 <p class="text-zinc-500">Don't have an account?</p>
                 <a class="font-bold text-slate-950" href="">Sign up for free</a>
