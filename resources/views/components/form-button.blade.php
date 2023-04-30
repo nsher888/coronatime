3f916932a0a8e23763fa626173a4af5aabdd396d
Nikoloz Shervashidze <51024662+nsher888@users.noreply.github.com>
2023-04-30 08:50:20 +0400

    style: change input error position to absolute

--- resources/views/components/form-button.blade.php
+++ resources/views/components/form-button.blade.php
@@ -1,5 +1,5 @@
 @props(['label'])
 
 <button
-    class="w-full py-4 mb-6 font-extrabold text-white transition-all duration-100 bg-green-500 rounded-lg hover:bg-green-600">{{
+    class="w-full py-4 mt-2 mb-6 font-extrabold text-white transition-all duration-100 bg-green-500 rounded-lg hover:bg-green-600">{{
     strtoupper($label) }}</button>
--- resources/views/components/form-input.blade.php
+++ resources/views/components/form-input.blade.php
@@ -1,13 +1,13 @@
 @props(['type', 'name', 'placeholder', 'label' => null,])
 
-<div class="flex flex-col gap-2 mb-4 md:mb-6">
+<div class="relative flex flex-col gap-2 mb-8 md:mb-9">
     <label class="font-bold text-slate-950" for="{{ $name }}">{{ $label ?? ucfirst($name) }}</label>
     <input type="{{ $type }}" name="{{ $name }}" id="{{ $name }}"
         class="py-4 pl-5 border border-gray-200 rounded-lg placeholder-zinc-500 text-slate-950 "
         placeholder="{{ $placeholder }}" value="{{ old($name) }}">
 
     @error($name)
-    <div class="flex items-center gap-2">
+    <div class="absolute flex items-center gap-2 -bottom-8">
         <img src="{{ asset('images/error-icon.png') }}" alt="error">
         <p class="text-sm text-red-700">{{ $message }}</p>
     </div>
