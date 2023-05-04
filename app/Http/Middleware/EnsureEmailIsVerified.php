b3370207e8dcbb3ae2101c27471b87bd48423066
Nikoloz Shervashidze <51024662+nsher888@users.noreply.github.com>
2023-05-04 14:04:19 +0400

    fix: delete comments

--- app/Http/Middleware/EnsureEmailIsVerified.php
+++ app/Http/Middleware/EnsureEmailIsVerified.php
@@ -9,14 +9,6 @@ use Illuminate\Support\Facades\URL;
 
 class EnsureEmailIsVerified
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  $redirectToRoute
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse|null
-     */
     public function handle($request, Closure $next, $redirectToRoute = null)
     {
         if (! $request->user() ||
