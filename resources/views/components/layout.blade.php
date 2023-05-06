c3ab642d0fc134bdf4ede88db4fd1f615b7fa6e1
Nikoloz Shervashidze <51024662+nsher888@users.noreply.github.com>
2023-05-06 17:37:56 +0400

    chore: add page title and favicon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <title>Coronatime</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('/images/icon.png') }}">
 </head>
 
 <body {{ $attributes->merge(['class' => 'w-full min-h-screen']) }}>
