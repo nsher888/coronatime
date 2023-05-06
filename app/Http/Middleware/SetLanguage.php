9ac6eff21c6e14aa2adcf2784bdbb86beaea523d
Nikoloz Shervashidze <51024662+nsher888@users.noreply.github.com>
2023-05-06 17:00:54 +0400

    fix: limit locales to en and ka

--- app/Http/Middleware/SetLanguage.php
+++ app/Http/Middleware/SetLanguage.php
@@ -5,13 +5,22 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Redirect;
 use Symfony\Component\HttpFoundation\Response;
 
 class SetLanguage
 {
+    protected $allowedLanguages = ['en', 'ka'];
+
     public function handle(Request $request, Closure $next): Response
     {
-        app()->setLocale($request->language);
+        $language = $request->language;
+
+        if (!in_array($language, $this->allowedLanguages)) {
+            return Redirect::back();
+        }
+
+        app()->setLocale($language);
 
         return $next($request);
     }
